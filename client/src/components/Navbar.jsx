3bff93745afdc73095eadf03846ad971e1903eea
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-15 20:40:04 +0800

    Subject:Body

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 // import {Container} from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+
 
 export const Navbar = () => {
 
     return (
         <>
             <Navbar className="bg-body-tertiary">
+                <Container>
                     <Navbar.Brand href="#home">
                         <img alt="" src="/img/logo.svg" width="30" height="30" className="d-inline-block align-top"/>
                         React Bootstrap
                     </Navbar.Brand>
+                </Container>
             </Navbar>
         </>
     )
