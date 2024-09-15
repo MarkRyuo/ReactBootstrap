7829d7bf4ebb175d47e91b71ad3f798140c081d7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-15 20:30:57 +0800

    Subject:Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Navbar } from './components/Navbar';
 
 function App() {
 
 
   return (
     <>
-      
+      <Navbar/>
     </>
   )
 }
