8c8cae9a5e0413818d66f961d06fc075518ad3e9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-15 20:29:32 +0800

    Subject:Body

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,8 +7,7 @@ export const Navbar = () => {
             <Navbar className="bg-body-tertiary">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img alt="" src="/img/logo.svg" width="30" height="30" className="d-inline-block align-top"
-            />{' '}
+                        <img alt="" src="/img/logo.svg" width="30" height="30" className="d-inline-block align-top"/>
             React Bootstrap
           </Navbar.Brand>
         </Container>
