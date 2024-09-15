f4db304298cba2f2631114bf379646987a4bb91c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-15 21:26:39 +0800

    Subject:Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Navbar1 } from './components/Navbar';
-import { Formlogin } from './components/Formlogin';
+import Formlogin from './components/Formlogin';
 
 function App() {
 
