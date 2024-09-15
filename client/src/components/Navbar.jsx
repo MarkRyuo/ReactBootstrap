a4521bef46e7db55f722c234ea6d2f181c2d58c8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-15 21:12:27 +0800

    Subject:Body

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 
 export const Navbar1 = () => {
 
     return (
         <>
-            <Navbar className="bg-body-tertiary">
-                <Container>
+            <Navbar className="bg-">
+                <Container xl>
                     <Navbar.Brand href="#">
                         <img alt="" src="/client/src/assets/react.svg" width="30" height="30" className=""/>
                         React Bootstrap
                     </Navbar.Brand>
+                    <Button variant="outline-primary">Download App</Button>{' '}
                 </Container>
             </Navbar>
         </>
