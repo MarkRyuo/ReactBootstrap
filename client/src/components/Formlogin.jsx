5347568e919a713676d3bf292fe04d87a8b561c0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 08:58:09 +0800

    Subject:Body

--- client/src/components/Formlogin.jsx
+++ client/src/components/Formlogin.jsx
@@ -1,20 +1,20 @@
 
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import './Formlogin.css'
+import './Formlogin.css' ;
 
 function Formlogin() {
     return (
         <>  
-            <FloatingLabel
+            <FloatingLabel 
                 controlId="floatingInput"
                 label="Email address"
-                className="mb-3">
+                className="mb-3 floating-label">
 
-            <Form.Control type="email" placeholder="name@example.com" />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingPassword" label="Password">
-            <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="email" placeholder="name@example.com" />
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" />
             </FloatingLabel>
         </>
     );
