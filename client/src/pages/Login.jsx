d76607d2913809588fb1af8e19fb8130cf9e483c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 10:35:47 +0800

    Subject:Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,21 +4,25 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './Login.css' ;
 import ReactLogo from '../assets/react.svg'
+import Button from 'react-bootstrap/Button';
 
 export const Login = () => {
 
     return (
         <>
 
-            <Container lg className="Container-login">
-                <Row className="text-center " >
+            <Container lg className="Container-login mt-4">
+                <Row className="text-center" >
                     <Col lg={12}>
-                        <img src={ReactLogo} alt="Error Image"></img>
+                        <img src={ReactLogo} alt="Error Image" width={"100px"}></img>
                     </Col>
-                    <Col lg={12}>
-                        <div>
+                    <Col lg={12} className="d-flex justify-content-center">
+                        <div style={{width:"300px"}}>
                             <Formlogin/> 
                         </div>   
+                    </Col>
+                    <Col lg={12}>
+                        <Button variant="primary">Login</Button>
                     </Col>
                 </Row>
             </Container>
