afd32a2e4216c8fb932097e5cb73e7e3acc17206
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 11:03:05 +0800

    Subject:Body

--- client/src/components/Formlogin.jsx
+++ client/src/components/Formlogin.jsx
@@ -12,7 +12,7 @@ function Formlogin() {
                     label="Email address"
                     className="mb-3 ">
 
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control type="email" placeholder="name@example.com"/>
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingPassword" label="Password">
                     <Form.Control type="password" placeholder="Password" />
