f4895246a50ea81db1888a09c1ddfae18506495a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 09:21:23 +0800

    Subject:Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/esm/Container"
 import Formlogin from "../components/Formlogin"
-import './Login.css'
+import './Login.css' ;
 
 export const Login = () => {
 
     return (
         <>
 
-            <Container lg>
+            <Container lg className="Container-login">
                 <Formlogin/>
             </Container>
 
