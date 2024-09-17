3b155d0d9a3a4e2ea0567d0967679eee129f7e0b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 10:15:57 +0800

    Subject: Body

--- client/src/components/NavDashboard.jsx
+++ client/src/components/NavDashboard.jsx
@@ -1,8 +1,13 @@
 import { Button, Container, Navbar, Offcanvas } from 'react-bootstrap'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react';
 
 
 export const NavDashboard = () => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
