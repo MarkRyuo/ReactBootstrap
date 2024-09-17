f000186ff6ffb9027ba706e1a72660c4f9bf8f6a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 10:20:09 +0800

    Subject: Body

--- client/src/components/NavDashboard.jsx
+++ client/src/components/NavDashboard.jsx
@@ -22,7 +22,7 @@ export const NavDashboard = () => {
                                 <Button variant="primary">Primary</Button>{' '}
                                 <Button variant="secondary">Secondary</Button>{' '}
                                 <Button variant="success">Success</Button>{' '}
-                                <Button variant="warning">Warning</Button>{' '}
+                                <Button variant="warning">Warning</Button>
                                 <Button variant="danger">Danger</Button>
                                 <Button variant="info">Info</Button>
                                 <Button variant="light">Light</Button>
