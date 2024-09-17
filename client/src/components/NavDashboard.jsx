73b88904923a5a6088876defb37d85c3d3a8410b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 09:35:27 +0800

    Subject: Body

---
+++ client/src/components/NavDashboard.jsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+export const NavDashboard = () => {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ export const Dashboard = () => {
 
     return (
         <>
-        
+            
         </>
     )
 }
