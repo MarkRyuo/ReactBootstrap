3d416feb847eb2efd7a9655f5eda588b99759e40
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 09:36:05 +0800

    Subject: Body

--- client/src/components/NavDashboard.jsx
+++ client/src/components/NavDashboard.jsx
@@ -6,7 +6,11 @@ export const NavDashboard = () => {
 
     return (
         <>
-            
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand>Brand text</Navbar.Brand>
+                </Container>
+            </Navbar>
         </>
     )
 }
