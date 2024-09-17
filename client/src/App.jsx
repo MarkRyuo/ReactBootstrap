222419aecf7485cb45bd8c64d055ea2f2ca97ee0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 09:50:07 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Login } from './pages/Login';
-import (BrowserRouter as Router, Routes, Route) from "react-router-dom" ;
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom" ;
 
 function App() {
 
 
   return (
-    <>
-      
-      <Login/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+    </Router>
   )
 }
 
