82dc405aba88b7b3f65f47cca786c3b9c55167cd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 09:42:41 +0800

    Subject: Body

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,8 +5,10 @@ import {BrowserRouter as Router, Routes, Route } from "react-router-dom" ;
 export const Dashboard = () => {
 
     return (
-        <>
-
-        </>
+        <Router>
+            <Routes>
+                
+            </Routes>
+        </Router>
     )
 }
