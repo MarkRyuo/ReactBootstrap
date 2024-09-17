3928af2a91d56330d5d3e6865076d11d77eef49c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 10:20:16 +0800

    Subject: Body

--- client/src/components/NavDashboard.jsx
+++ client/src/components/NavDashboard.jsx
@@ -20,8 +20,8 @@ export const NavDashboard = () => {
                             </Offcanvas.Header>
                             <Offcanvas.Body>
                                 <Button variant="primary">Primary</Button>{' '}
-                                <Button variant="secondary">Secondary</Button>{' '}
-                                <Button variant="success">Success</Button>{' '}
+                                <Button variant="secondary">Secondary</Button>
+                                <Button variant="success">Success</Button>
                                 <Button variant="warning">Warning</Button>
                                 <Button variant="danger">Danger</Button>
                                 <Button variant="info">Info</Button>
