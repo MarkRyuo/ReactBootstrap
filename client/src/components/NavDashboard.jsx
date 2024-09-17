e7d9de72141f8c0129da95f51a5895dacbed90da
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-17 10:17:51 +0800

    Subject: Body

--- client/src/components/NavDashboard.jsx
+++ client/src/components/NavDashboard.jsx
@@ -14,6 +14,15 @@ export const NavDashboard = () => {
             <Navbar className="bg-body-tertiary">
                 <Container>
                     <Button variant="outline-primary" onClick={handleShow}><GiHamburgerMenu /></Button>
+                        <Offcanvas show={show} onHide={handleClose}>
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                Some text as placeholder. In real life you can have the elements you
+                                have chosen. Like, text, images, lists, etc.
+                            </Offcanvas.Body>
+                        </Offcanvas>
                     <Navbar.Brand>SIMS</Navbar.Brand>
                 </Container>
             </Navbar>
