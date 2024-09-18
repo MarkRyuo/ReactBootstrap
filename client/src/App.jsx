413a5c2242d6472195be4be0f7c711a1e5262734
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-18 17:14:54 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Login } from './pages/Login';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom" ;
-// import { Dashboard } from './pages/Dashboard';
+
 import Product from './pages/Product';
 import Dash from './components/Dash';
 import { Dashboard } from './pages/Dashboard';
