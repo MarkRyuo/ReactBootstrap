121bfb64138bfcca279c981f1e300a78799bb731
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-18 17:28:13 +0800

    Subject: Body

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,7 @@
 import { NavDashboard } from "../components/NavDashboard";
 
 import {Outlet } from "react-router-dom" ;
-import Product from "./Product";
 import { Container } from "react-bootstrap";
-import Dash from "../components/Dash";
 
 
 export const Dashboard = () => {
